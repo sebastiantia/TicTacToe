f16a817fadb2dfca958e0cd8386d2ffcb48310fc
sebastiantia <75666019+sebastiantia@users.noreply.github.com>
2021-09-14 00:28:14 -0400

    Create main.py
    Define useful parameters

---
+++ main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import random
+import time
+import numpy as np
+from PIL import ImageTk,Image
+
+
+size_of_board = 600
+rows = 10
+cols = 10
+DELAY = 100
+snake_initial_length = 3
+symbol_size = (size_of_board / 3 - size_of_board / 8) / 2
+symbol_thickness = 2
+RED_COLOR = "#EE4035"
+BLUE_COLOR = "#0492CF"
+Green_color = "#7BC043"
+
+BLUE_COLOR_LIGHT = '#67B0CF'
+RED_COLOR_LIGHT = '#EE7E77'
